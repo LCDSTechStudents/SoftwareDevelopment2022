b8cbc2594b90d28b2232a9f184abc264785b7669
Andreas Jaramillo <jaramilloan@lancastercountryday.org>
2022-03-13 23:25:06 -0400

    Text Box working Added a working button

--- app/src/main/java/com/example/photolang/LoginPages.java
+++ app/src/main/java/com/example/photolang/LoginPages.java
@@ -1,8 +1,8 @@
 package com.example.photolang;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +10,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONException;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -24,8 +23,7 @@ import java.util.Map;
 
 public class LoginPages extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
       //  final TextView textView = (TextView) findViewById(R.id.SendResponse);
@@ -35,37 +33,34 @@ public class LoginPages extends AppCompatActivity {
         String user_input = user.getText().toString();
         String pass_input = pass.getText().toString();
         //For Send button
-        Button button = (Button) findViewById(R.id.send);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-               String user_text = user.getText().toString();
-               String pass_text = pass.getText().toString();
-               if (user_text.isEmpty() || pass_text.isEmpty()){
-                   return;
-               }
+        Button sendbutton = (Button) findViewById(R.id.send);
+        sendbutton.setOnClickListener((View.OnClickListener) this){
+        public void onClick(View view){
+                Toast.makeText(LoginPages.this, "Button is working", Toast.LENGTH_LONG).show();
+                String user_text = user.getText().toString();
+                String pass_text = pass.getText().toString();
+
 
                 RequestQueue queue = Volley.newRequestQueue(LoginPages.this);
-                String url ="http://www.lancastertsa.com:1002/v1/auth/login";
+                String url = "http://www.lancastertsa.com:1002/v1/auth/login";
 
 // Request a string response from the provided URL.
                 StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
-                                // Display the first 500 characters of the response string.
-                              Toast.makeText(LoginPages.this, "Response: " + response, Toast.LENGTH_LONG).show();
-                              try {
-                                  Root resp = JSON.parseObject(response, Root.class);
-                                  Log.d(resp.data.toString(), "onResponse: ");
-                                  if(resp.code == -1){
-                                      Log.d(resp.data.toString(), "onResponse: code -1");
-                                  }
-                              }catch (JSONException e){
-                                  //switch e here
-                                  Log.d("Parsing error", e.toString());
-                              }finally{
-                                  Log.d("On error", response.toString());
-                              }
+                                Toast.makeText(LoginPages.this, "Response: " + response, Toast.LENGTH_LONG).show();
+                                try {
+                                    Root resp = JSON.parseObject(response, Root.class);
+                                    Log.d(resp.data.toString(), "onResponse: ");
+                                    if (resp.code == -1) {
+                                        Log.d(resp.data.toString(), "onResponse: code -1");
+                                    } else {
+                                        opencameraActivity();
+                                    }
+                                } finally {
+                                    Log.d("On error", response.toString());
+                                }
 
                             }
                         },
@@ -73,38 +68,36 @@ public class LoginPages extends AppCompatActivity {
 
                             @Override
                             public void onErrorResponse(VolleyError error) {
-                                Toast.makeText(LoginPages.this,"Error: " + error.getMessage(), Toast.LENGTH_LONG).show();
+                                Toast.makeText(LoginPages.this, "Error: " + error.getMessage(), Toast.LENGTH_LONG).show();
                             }
-                        }){
+                        }) {
                     @Override
-                    protected Map<String, String> getParams(){
+                    protected Map<String, String> getParams() {
                         Map<String, String> paramV = new HashMap<>();
                         paramV.put(user_input, pass_input);
                         return paramV;
                     }
                 };
-               queue.add(stringRequest);
-               queue.start();
+                queue.add(stringRequest);
+                queue.start();
 
-            //    opencameraActivity();
             }
-        });
+            }
+        ;
 
       //Second button
 
-        Button button2 = (Button) findViewById(R.id.register);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                openregisterActivity();
+        Button register_button = (Button) findViewById(R.id.register);
+        register_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent_register= new Intent(LoginPages.this, register.class);
+                startActivity(intent_register);
             }
         });
-
     }
 
-    private void openregisterActivity() {
-        Intent intent_camera= new Intent(this, register.class);
-        startActivity(intent_camera);
-    }
+
 
 
     private void opencameraActivity() {
--- app/src/main/java/com/example/photolang/MainActivity.java
+++ app/src/main/java/com/example/photolang/MainActivity.java
@@ -1,39 +1,30 @@
 package com.example.photolang;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.navigation.ui.AppBarConfiguration;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-//import com.example.photolang.databinding.ActivityMainBinding;
+import com.example.photolang.databinding.LoginPageBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private Object Loginpage;
+    //private Object Loginpage;
 
-    //  private ActivityMainBinding binding;
+    LoginPageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = LoginPageBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
         setContentView(binding.getRoot());
-        //Loginpage = new Loginpage();
+
+
+
+
+
+
 
        // BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -45,6 +36,5 @@ public class MainActivity extends AppCompatActivity {
        // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         //NavigationUI.setupWithNavController(binding.nav_view, navController);
     }
-
 
 }
